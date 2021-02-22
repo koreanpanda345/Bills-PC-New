3700d1f55d8437b8672669d814e667fb7ec900ec
Cody Spratford <koreanpanda345@gmail.com>
2021-02-22 13:50:56 -0800

    Moving the bot to heroku

--- src/commands/draft/addtoqueue.ts
+++ src/commands/draft/addtoqueue.ts
@@ -13,30 +13,31 @@ export class AddtoqueueCommand implements ICommand {
 	usage = ["b!addqueue <league prefix> <pokemon>"];
 	
 	invoke = async(ctx: CommandContext) => {
-		let prefix = ctx.args.shift()?.toLowerCase();
-		DraftTimer.findOne({prefix}, (err: CallbackError, record: IDraftTimer) => {
-			if(!record) return ctx.sendMessage(`There is no league that has this prefix.`);
-			if(!record.players.find(x => x.userId === ctx.userId)) return ctx.sendMessage("Are not in this league. If this is a mistaken, then tell one of your league liaisons.");
-			let player = record.players.find(x => x.userId === ctx.userId);
-			let pokemon = ctx.args.join(" ");
-			pokemon = getNamingConvention(pokemon);
-			let check = Dex.getSpecies(pokemon.toLowerCase().trim());
-			if(!check) return ctx.sendMessage(`That is not a valid pokemon.`);
-			let found = record.pokemon.find(x => x === pokemon);
-			if(found) return ctx.sendMessage(`${pokemon} has already been drafted by ${record.players.find(x => x.pokemon.includes(pokemon))}!`);
-			if(player?.queue.includes(pokemon)) return ctx.sendMessage("This pokemon is already in queue.");
 
-			player?.queue.push(pokemon);
-			
-			let embed = new MessageEmbed().setTitle(`Added to Queue for ${record.leagueName}`);
-			embed.setDescription(`To remove a pokemon from the queue, use the command \`removequeue\`.`);
-			player?.queue.forEach(x => {
-				embed.addField(`In queue(In ${player?.queue.findIndex(y => y === x)} Rounds): `, x);
+		return await new Promise((resolve) => {
+			let prefix = ctx.args.shift()?.toLowerCase();
+			DraftTimer.findOne({prefix}, (err: CallbackError, record: IDraftTimer) => {
+				if(!record) return ctx.sendMessage(`There is no league that has this prefix.`);
+				if(!record.players.find(x => x.userId === ctx.userId)) return ctx.sendMessage("Are not in this league. If this is a mistaken, then tell one of your league liaisons.");
+				let player = record.players.find(x => x.userId === ctx.userId);
+				let pokemon = ctx.args.join(" ");
+				pokemon = getNamingConvention(pokemon);
+				let check = Dex.getSpecies(pokemon.toLowerCase().trim());
+				if(!check) return ctx.sendMessage(`That is not a valid pokemon.`);
+				let found = record.pokemon.find(x => x === pokemon);
+				if(found) return ctx.sendMessage(`${pokemon} has already been drafted by ${record.players.find(x => x.pokemon.includes(pokemon))}!`);
+				if(player?.queue.includes(pokemon)) return ctx.sendMessage("This pokemon is already in queue.");
+	
+				player?.queue.push(pokemon);
+				
+				let embed = new MessageEmbed().setTitle(`Added to Queue for ${record.leagueName}`);
+				embed.setDescription(`To remove a pokemon from the queue, use the command \`removequeue\`.`);
+				player?.queue.forEach(x => {
+					embed.addField(`In queue(In ${player?.queue.findIndex(y => y === x)} Rounds): `, x);
+				});
+				record.save().catch(error => console.error(error));
+				ctx.sendMessage(embed);
 			});
-			record.save().catch(error => console.error(error));
-			return ctx.sendMessage(embed);
-			
 		});
-		
 	};
 }
